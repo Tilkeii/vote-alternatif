d9262181e62dd0414afe367060ae9baf42970fe5
Tilkeii <florian@gouarin.info>
2019-12-03 20:23:29 +0100

    update
    Co-authored-by: Florian Richard <34646731+fricha05@users.noreply.github.com>
    Co-authored-by: nntucao <ngtu.caong@gmail.com>
    Co-authored-by: Nicolas Barseyni <nicolas.barseyni@protonmail.com>

--- blockchain/contracts/AlternativeVote.sol
+++ blockchain/contracts/AlternativeVote.sol
@@ -1,24 +1,125 @@
 pragma solidity >=0.4.21 < 0.6.0;
+pragma experimental ABIEncoderV2;
 
 import "./Ownable.sol";
 
 
-contract AlternativeVote is Ownable{
-    Candidates[5] public candidates;
-    Candidates[5] public votesList;
+contract AlternativeVote is Ownable {
+    Phase currentPhase;
 
-    struct Candidates{
-        string firstName;
-        string lastName;
-        string description;
-        address candidate;
+    /**
+        liste d'adresse des candidats
+        candidatesList = ["0xJHJK13KJX", "0xLKJKHK3KJX"]
+     */
+    address[] candidatesList;
+    /**
+        {
+            liste_votants : [liste_candidat]
+            "0xKJHKJH": ["0xJHJK13KJX", "0xLKJKHK3KJX"],
+            "0xJBHJ256KJH" : ["0xLKJKHK3KJX", "0xJHJK13KJX"]
+        }
+     */
+    mapping(address => address[]) votersAddressMapping;
+    // liste_votants : ["0xKJHKJH", "0xJBHJ256KJH"] pour parser le mapping
+    address[] votersAddress;
+
+    enum Phase {
+        REGISTER,
+        OPEN,
+        CLOSED
     }
-    
+
     constructor() public {
+        currentPhase = Phase.REGISTER;
     }
-    
-    function giveVotesList(Candidates[5] _votesList) public {
-        this.votesList = _votesList;
+
+    function registerCandidate() public onlyRegisterPhase {
+        candidatesList.push(msg.sender);
     }
+
+    /**
+        Permet à un utilisateur de declarer son vote
+        Son vote est un tableau avec tous les candidats
+        classé de son préféré à son pire
+     */
+    function giveVotesList(address[] memory _votesList) public onlyOpenPhase {
+        votersAddressMapping[msg.sender] = _votesList;
+        votersAddress.push(msg.sender);
+    }
+
+    function closeVoteSession() public onlyOwner {
+        currentPhase = Phase.CLOSED;
+        processvotersAddressMapping();
+    }
+
+    //calcule les résultats
+    function processVotersAddressMapping() internal {
+        uint roundNumber = candidatesList.length;
+        
+        for(uint round = 0; round < roundNumber; round++) {
+            for(uint voter = 0; voter < votersAddress.length; voter++) {
+
+            }
+
+        }
+
+    }
+
+    /**
+        @param array
+        @return index of smaller int in array
+     */
+    function smallest(uint[] array) returns(uint){
+        uint min = 0;
+        for (uint i=1; i<array.length-1; i++){
+            if(array[i] < array[min]) min = i; 
+        }
+        return min;
+    }
+
+    /**
+        @param index
+        @param array
+        @return array without the element at the given index
+     */
+    function remove(uint index, address[] memory array) internal returns(address[] memory) {
+        if (index >= array.length) return;
+
+        for (uint i = index; i < array.length - 1; i++){
+            array[i] = array[i + 1];
+        }
+        // delete array[array.length-1];
+        array.length--;
+        return array;
+    }
+
+    /**
+        @param
+        @return
+     */
+    function setArrayForNextRound(uint[] memory array) internal returns(uint[] memory) {
+        for(uint i = 0; i < array.length - 1; i++){
+            array[i] = 0;
+        }
+        return array.length--;
+    }
+
+    /**
     
+     */
+
+    //renvoie le vote de l'utilisateur
+    // function getMyVote() public {
+
+    // }
+
+    modifier onlyRegisterPhase() {
+        require(currentPhase == Phase.REGISTER, "Current phase is not REGISTER");
+        _;
+    }
+
+    modifier onlyOpenPhase() {
+        require(currentPhase == Phase.OPEN, "Current phase is not Open");
+        _;
+    }
 }
--- blockchain/contracts/Ownable.sol
+++ blockchain/contracts/Ownable.sol
@@ -1,6 +1,5 @@
-pragma solidity >=0.4.21 <0.6.0;
+pragma solidity ^0.5.0;
 
-import "./Context.sol";
 /**
  * @dev Contract module which provides a basic access control mechanism, where
  * there is an account (an owner) that can be granted exclusive access to
@@ -10,7 +9,7 @@ import "./Context.sol";
  * `onlyOwner`, which can be applied to your functions to restrict their use to
  * the owner.
  */
-contract Ownable is Context {
+contract Ownable {
     address private _owner;
 
     event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
@@ -19,15 +18,14 @@ contract Ownable is Context {
      * @dev Initializes the contract setting the deployer as the initial owner.
      */
     constructor () internal {
-        address msgSender = _msgSender();
-        _owner = msgSender;
-        emit OwnershipTransferred(address(0), msgSender);
+        _owner = msg.sender;
+        emit OwnershipTransferred(address(0), _owner);
     }
 
     /**
      * @dev Returns the address of the current owner.
      */
-    function getOwner() public view returns (address) {
+    function owner() public view returns (address) {
         return _owner;
     }
 
@@ -43,7 +41,7 @@ contract Ownable is Context {
      * @dev Returns true if the caller is the current owner.
      */
     function isOwner() public view returns (bool) {
-        return _msgSender() == _owner;
+        return msg.sender == _owner;
     }
 
     /**
